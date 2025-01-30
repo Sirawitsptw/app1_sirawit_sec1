e57d2002682150d5cf357bdee4204b556a73debd
Sirawitsptw <152370451+Sirawitsptw@users.noreply.github.com>
2025-01-30 09:58:45 +0700

    Context

--- src/App.js
+++ src/App.js
@@ -2,34 +2,23 @@
 import './App.css';
 import React from 'react';
 import './style.css'
-import{Header , Content , Footer} from './func-components';
-import Calendar from './class-components';
-import { Button } from './class-components';
-import { Calculator2 } from './calculator';
-import { EventData2 } from './even-data';
-import RefsFunc from './refs-func';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
+//import{Header , Content , Footer} from './func-components';
+//import Calendar from './class-components';
+//import { Button } from './class-components';
+//import { Calculator2 } from './calculator';
+//import { EventData2 } from './even-data';
+//import RefsFunc from './refs-func';
+//import RefsArray from './refs-array';
+//import MessageBox from './state-func';
+import Content from './context-content';
+import { userContext } from './context';
 
 function App() {
   return(
     <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-      <p><center>-----------------------------------------</center></p>
-      <p><center><Button/></center></p>
-      <p><center>-----------------------------------------</center></p>
-      <Calculator2/>
-      <p><center>-----------------------------------------</center></p>
-      <EventData2/>
-      <p><center>-----------------------------------------</center></p>
-      <RefsFunc/>
-      <p><center>-----------------------------------------</center></p>
-      <RefsArray/>
-      <p><center>-----------------------------------------</center></p>
-      <MessageBox/>
+      <userContext.Provider value = {'Palm'}>
+        <Content />
+      </userContext.Provider>
     </>
   );
 }
