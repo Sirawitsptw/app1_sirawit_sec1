595182e5670b7a050d966012d7e21514e06516e2
Sirawitsptw <152370451+Sirawitsptw@users.noreply.github.com>
2025-02-20 11:51:43 +0700

    Update form-select-file.js

--- src/form-select-file.js
+++ src/form-select-file.js
@@ -49,7 +49,7 @@ export default function FormSelectFile() {
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="file" className="form-label">เลือกไฟล์</label>
-                    <input type="file" id="file" accept="image"
+                    <input type="file" id="file" accept="image/*"
                         className="form-control form-control-sm" ref={inputFile} multiple/>
                 </div>
                 <div className="text-center mt-4">
